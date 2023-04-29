eb66dd991345bcdb587246e2df3e5a026487d5fa
Ladányi Bence <ladanyi.bence96@gmail.com>
2023-04-29 17:56:59 +0200

    swiper module added

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,28 +1,41 @@
 import './Body.css';
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
 
 import Navigation from './Navigation.js';
 import FeatureItem from './FeatureItem';
 import SpellItem from './SpellItem';
 
 function Body(props) {
+  register();
+  const swiperElRef = useRef(null);
+
   const {selectedChar, characterAbilities} = props;
 
   const [selectedAbility, setSelectedAbility] = useState('features');
   const [concentration, setConcentraton] = useState({active: false, spellId: 0});
 
   useEffect(() => {
-    const abilityWrapperList = document.querySelectorAll('.ability-wrapper');
-    abilityWrapperList.forEach(abilityWrapper => {
-      if (abilityWrapper.classList.contains(selectedAbility + '-wrapper')) {
-        abilityWrapper.classList.add('active');
+    if (selectedAbility === 'features') {
+      swiperElRef.current.swiper.slidePrev();
+    }
+
+    if (selectedAbility === 'spells') {
+      swiperElRef.current.swiper.slideNext();
+    }
+  }, [selectedAbility]);
+
+  useEffect(() => {
+    swiperElRef.current.addEventListener('slidechange', (e) => {
+      if (e.detail[0].activeIndex === 0) {
+        setSelectedAbility('features');
       }
 
-      if (!abilityWrapper.classList.contains(selectedAbility + '-wrapper')) {
-        abilityWrapper.classList.remove('active');
+      if (e.detail[0].activeIndex === 1) {
+        setSelectedAbility('spells');
       }
-    })
-  }, [selectedAbility]);
+    });
+  }, []);
 
   const selectedCharId = selectedChar.id;
   let featureList = [];
@@ -37,11 +50,24 @@ function Body(props) {
   return (
     <div className='body-wrapper'>
       <Navigation selectedAbility={selectedAbility} updateSelectedAbilityHandler={setSelectedAbility} concentration={concentration.active}/>
-      <div className='ability-wrapper features-wrapper active'>
-        {featureList.map((feature, i) => <FeatureItem key={"feature-" + i} featureId={feature}/>)}
-      </div>
-      <div className='ability-wrapper spells-wrapper'>
-        {spellList.map((spell, i) => <SpellItem key={"spell-" + i} spellId={spell} concentration={{...concentration}} updateConcentration={setConcentraton}/>)}
+      <div className='swiper-wrapper-div'>
+        <swiper-container
+          ref={swiperElRef}
+          slides-per-view="1"
+          navigation="false"
+          pagination="false"
+          threshold="10">
+          <swiper-slide>
+            <div className='ability-wrapper features-wrapper'>
+              {featureList.map((feature, i) => <FeatureItem key={"feature-" + i} featureId={feature}/>)}
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+          <div className='ability-wrapper spells-wrapper'>
+            {spellList.map((spell, i) => <SpellItem key={"spell-" + i} spellId={spell} concentration={{...concentration}} updateConcentration={setConcentraton}/>)}
+          </div>
+          </swiper-slide>
+        </swiper-container>
       </div>
     </div>
   );
