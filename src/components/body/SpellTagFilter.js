3a0b3b39f281315c39105a31d5e4ad3b77344360
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2024-11-29 16:48:07 +0100

    fix closing spelltag filters

--- src/components/body/SpellTagFilter.js
+++ src/components/body/SpellTagFilter.js
@@ -5,17 +5,25 @@ const SpellTagFilter = ({
   activeSpellTagFilters,
   setActiveSpellTagFilters
 }) => {
+  const activeSpellTagFiltersRef = useRef({...activeSpellTagFilters});
   const filterButtonRef = useRef(null);
 
   const tagFilterClickHandler = (e) => {
     const type = e.target.dataset.type
-    
-    setActiveSpellTagFilters((prevState) => {
-      const newState = {...prevState};
-      newState[type] = !prevState[type];
 
-      return newState;
-    });
+    const newRef = {...activeSpellTagFiltersRef.current};
+    newRef[type] = !activeSpellTagFiltersRef.current[type];
+
+    activeSpellTagFiltersRef.current = newRef;
+
+    e.target.classList.toggle('active');
+    
+    // setActiveSpellTagFilters((prevState) => {
+    //   const newState = {...prevState};
+    //   newState[type] = !prevState[type];
+
+    //   return newState;
+    // });
   }
 
   const filterButtonClickHandler = () => {
@@ -24,30 +32,41 @@ const SpellTagFilter = ({
     }
   }
 
+  const updateFilterState = () => {
+    if (activeSpellTagFiltersRef.current) {
+      setTimeout(() => {
+        setActiveSpellTagFilters(activeSpellTagFiltersRef.current)
+      }, 200);
+    }
+  }
+
   return (
     <div className='extra-spell-filter-wrapper' ref={filterButtonRef}>
       <i id="extra-spell-filter-open-btn" className="fa-solid fa-filter" onClick={filterButtonClickHandler}></i>
-      <i id="extra-spell-filter-close-btn" className="fa-solid fa-arrow-right" onClick={filterButtonClickHandler}></i>
+      <i id="extra-spell-filter-close-btn" className="fa-solid fa-arrow-right" onClick={() => {
+        filterButtonClickHandler();
+        updateFilterState();
+      }}></i>
       <div className='extra-spell-filter-backdrop' onClick={filterButtonClickHandler}></div>
       <div className="extra-spell-filter-panel">
         <div className='extra-spell-filter-group spell-tag-filters'>
           <div className='extra-spell-filter-title'>Spell Types</div>
           <div
-            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFilters.support && 'active'}`}
+            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFiltersRef.current?.support && 'active'}`}
             data-type='support'
             onClick={tagFilterClickHandler}
           >
             Support
           </div>
           <div
-            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFilters.combat && 'active'}`}
+            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFiltersRef.current?.combat && 'active'}`}
             data-type='combat'
             onClick={tagFilterClickHandler}
           >
             Combat
           </div>
           <div
-            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFilters.utility && 'active'}`}
+            className={`extra-spell-filter-btn spell-tag-filter ${activeSpellTagFiltersRef.current?.utility && 'active'}`}
             data-type='utility'
             onClick={tagFilterClickHandler}
           >
