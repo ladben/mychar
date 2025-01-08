68576f2603177021e56e00da5f7e3509d79f31a6
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2025-01-08 17:37:14 +0100

    Adding header image and gif

--- src/components/header/Heading.js
+++ src/components/header/Heading.js
@@ -3,9 +3,18 @@ import './Heading.css';
 import CharChooser from './CharChooser';
 
 function Heading(props) {
+
+  const runAnimation = (e) => {
+    e.target.classList.add('animating');
+
+    setTimeout(() => {
+      e.target.classList.remove('animating');
+    }, 5200);
+  }
+
   return (
     <div className='header-wrapper'>
-      <div className='header'>MyChar</div>
+      <div className='header' onClick={runAnimation}>MyDndChar</div>
       <CharChooser {...props}/>
     </div>
   );
