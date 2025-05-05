fff4eae3edd1867989f1429637ac5c35f33014b5
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2025-05-05 17:36:06 +0200

    update filter design

--- src/components/body/SpellFilterGroup/SpellFilterGroup.js
+++ src/components/body/SpellFilterGroup/SpellFilterGroup.js
@@ -2,16 +2,18 @@ const SpellFilterGroup = ({groupName, groupClassName, filterType, options, activ
     return (
         <div className={`extra-spell-filter-group ${groupClassName}`}>
           <div className='extra-spell-filter-title'>{groupName}</div>
-          {options.map((option, index) => (
-            <div
-                key={index}
-                className={`extra-spell-filter-btn spell-${filterType}-filter ${activeSpellFilters && activeSpellFilters[option.toLowerCase()] ? 'active' : ''}`}
-                {...{[`data-${filterType}`]: option.toLowerCase()}}
-                onClick={filterClickHandler}
-            >
-                {option}
-            </div>
-          ))}
+          <div className='extra-spell-filter-options'>
+            {options.map((option, index) => (
+              <div
+                  key={index}
+                  className={`extra-spell-filter-btn spell-${filterType}-filter ${activeSpellFilters && activeSpellFilters[option.toLowerCase()] ? 'active' : ''}`}
+                  {...{[`data-${filterType}`]: option.toLowerCase()}}
+                  onClick={filterClickHandler}
+              >
+                  {option}
+              </div>
+            ))}
+          </div>
         </div>
     );
 }
--- src/components/body/SpellTagFilter.js
+++ src/components/body/SpellTagFilter.js
@@ -35,6 +35,7 @@ const SpellTagFilter = ({
   const filterButtonClickHandler = () => {
     if (filterButtonRef.current) {
       filterButtonRef.current.classList.toggle('open');
+      document.body.classList.toggle('no-scroll');
     }
   }
 
@@ -49,31 +50,34 @@ const SpellTagFilter = ({
   return (
     <div className='extra-spell-filter-wrapper' ref={filterButtonRef}>
       <i id="extra-spell-filter-open-btn" className="fa-solid fa-filter" onClick={filterButtonClickHandler}></i>
-      <i id="extra-spell-filter-close-btn" className="fa-solid fa-arrow-right" onClick={() => {
-        filterButtonClickHandler();
-        updateFilterState();
-      }}></i>
       <div className='extra-spell-filter-backdrop' onClick={() => {
         filterButtonClickHandler();
         updateFilterState();
       }}></div>
       <div className="extra-spell-filter-panel">
-        <SpellFilterGroup
-          groupName="Spell Types"
-          groupClassName="spell-tag-filters"
-          filterType="type"
-          options={["Support", "Combat", "Utility"]}
-          activeSpellFilters={activeSpellTagFiltersRef.current.type}
-          filterClickHandler={tagFilterClickHandler}
-        />
-        <SpellFilterGroup
-          groupName="Spell Levels"
-          groupClassName="spell-level-filters"
-          filterType="level"
-          options={["1st-level", "2nd-level", "3rd-level"]}
-          activeSpellFilters={activeSpellTagFiltersRef.current.level}
-          filterClickHandler={levelFilterClickHandler}
-        />
+        <div className="spell-filter-panel-title">Filter Spells</div>
+        <div className="spell-filter-option-groups">
+          <SpellFilterGroup
+            groupName="Spell Types"
+            groupClassName="spell-tag-filters"
+            filterType="type"
+            options={["Support", "Combat", "Utility"]}
+            activeSpellFilters={activeSpellTagFiltersRef.current.type}
+            filterClickHandler={tagFilterClickHandler}
+          />
+          <SpellFilterGroup
+            groupName="Spell Levels"
+            groupClassName="spell-level-filters"
+            filterType="level"
+            options={["1st-level", "2nd-level", "3rd-level"]}
+            activeSpellFilters={activeSpellTagFiltersRef.current.level}
+            filterClickHandler={levelFilterClickHandler}
+          />
+        </div>
+        <div className="apply-filters-button" onClick={() => {
+          filterButtonClickHandler();
+          updateFilterState();
+        }}> Apply Filters</div>
       </div>
     </div>
   );
